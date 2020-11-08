91fc4263350d28487aab7c6550f722d7e8a1b0f4
Amyre Polk <apolk427@gmail.com>
2020-11-08 16:31:34 -0600

    commit tree b88293160d4f291ef20578f6009c013b8d912ad0

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,15 @@ class Dog
         @name = name
         @breed = breed
     end
+    def name
+        @name
+    end
 
+    def breed=(the_dog_breed)
+        @breed=the_dog_breed
+    end
+
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -3,5 +3,15 @@ class Person
     def initialize(name)
      @name = name
     end
-     
+    def name
+        @name
+    end
+
+    def job=(the_person_job)
+        @job=the_person_job
+    end
+
+    def job
+        @job
+    end
  end
